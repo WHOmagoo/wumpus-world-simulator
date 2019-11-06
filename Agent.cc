e21f64ea2ebcba8408bf64c45f37b5d049791519
hmcgough <hugh.mcgough@wsu.edu>
2019-11-05 20:29:22 -0800

    Homework 9 full agent

--- Agent.cc
+++ Agent.cc
@@ -1,23 +1,68 @@
 // Agent.cc
 
 #include <iostream>
+#include <cstdio>
 #include "Agent.h"
+#include "Orientation.h"
 
 using namespace std;
 
+const int size = 5;
+
+double pitChance[size][size];
+
+int x = 0, y =0;
+
+Orientation o;
+
+class Orientation
+
 Agent::Agent ()
 {
-
+    o = Orientation.Right;
+    printf("Constructor");
+	for (auto &i : pitChance) {
+		for (double &j : i) {
+			j = .2;
+		}
+	}
 }
 
 Agent::~Agent ()
 {
-
+    printf("Destruct");
 }
 
 void Agent::Initialize ()
 {
+    printf("Initialize");
 
+}
+
+bool outOfBounds(int x, int y){
+    return x < 0 || x >= size || y < 0 || y >= size;
+}
+
+void findBestNeighbor(int *xRes, int *yRes){
+    int nX[] = {x - 1, x - 1, x + 1, x + 1};
+    int nY[] = {y - 1, y + 1, y - 1, y + 1};
+
+    double lowestAccpetable = 1;
+    double acceptableIndex = -1;
+
+    for(int i = 0; i < 4; i++){
+        if(outOfBounds(nX[i], nY[i])){
+            continue;
+        }
+
+        int curPitChance = pitChance[nX[i]][nY[i]];
+
+
+        if(lowestAccpetable > curPitChance && curPitChance < .5 && curPitChance > 0){
+
+        }
+
+    }
 }
 
 Action Agent::Process (Percept& percept)
@@ -25,6 +70,8 @@ Action Agent::Process (Percept& percept)
 	char c;
 	Action action;
 	bool validAction = false;
+
+    pitChance[x][y] = 0;
 
 	while (! validAction)
 	{
